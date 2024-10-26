e1b53cf9a673f6791f0c24477dda9963bf090abd
Sijan04 <98251827+Sijan04@users.noreply.github.com>
2024-10-27 01:37:14 +0600

    Add files via upload

---
+++ Number methods.js
@@ -0,0 +1,8 @@
+var Number = 20.0100;
+
+//num=toString(num)     Convert to string
+//num= parseFloat(num);
+
+console.log(Number.toFixed(2))  //after point how digit to shown
+
+//console.log(typeof(num));
