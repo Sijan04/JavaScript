48b401d07eb5a56a53d30986fe31ec2b98a7642d
Sijan04 <98251827+Sijan04@users.noreply.github.com>
2024-10-28 15:16:41 +0600

    Add files via upload

---
+++ Area.js
@@ -0,0 +1,10 @@
+var base= parseFloat(prompt("Enter Base= "));
+var height= parseFloat(prompt("Enter Height"));
+
+var area= base * height;    //area calculator
+                                  
+var Triangle= (base * height)/2;  //triangle calculator
+
+document.write("Area=" + area + "<br/>");
+
+document.write("Trinagle=" + Triangle);
