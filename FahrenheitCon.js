d051afd75c76bca751dbbb1fc4131c7c48450603
Sijan04 <98251827+Sijan04@users.noreply.github.com>
2024-10-28 15:32:40 +0600

    Add files via upload

---
+++ FahrenheitCon.js
@@ -0,0 +1,10 @@
+//var Fahrenheit= parseFloat(prompt("Enter Fahrenheit= "));
+var Celsius=parseFloat(prompt("Enter Celsius="));
+
+//var Celsius= (Fahrenheit - 32) * (5 / 9) ;    //Celsius to fahrenheit coversation
+
+var Fahrenheit=(Celsius * (9/5)) + 32;
+
+//document.write("Celsius= " + Celsius + "<br/>");
+
+document.write("Fahrenheit= " + Fahrenheit);
