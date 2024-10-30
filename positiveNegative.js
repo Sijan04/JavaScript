85556f13ebff65eea1de781a0354ac0d6ec9e4cd
Sijan04 <98251827+Sijan04@users.noreply.github.com>
2024-10-31 02:37:58 +0600

    Add files via upload

---
+++ positiveNegative.js
@@ -0,0 +1,20 @@
+var number= prompt("Enter a number");
+
+if (number>0)
+
+    console.log("positive");
+
+    
+else if (number<0)
+
+    console.log("Negative");
+
+
+
+else (number==0)
+
+        console.log("Zero");
+    
+            
+
+    
