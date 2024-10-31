87f2a93c943b198cda9b1d96fb6f0448af625107
Sijan04 <98251827+Sijan04@users.noreply.github.com>
2024-10-31 16:38:57 +0600

    Add files via upload

---
+++ LargeNumber.js
@@ -0,0 +1,14 @@
+var num1= prompt("Enter Number1:");
+var num2= prompt("Enter Number2:");
+var num3= prompt("Enter Number3:");
+
+if(num1 > num2 && num1 > num3)
+ document.write("Large Number=" +num1);
+
+else if(num2 > num1 && num2 > num3)
+    document.write("Large Number=" +num2);
+
+else
+ document.write("Large Number= " + num3);
+    
+    
