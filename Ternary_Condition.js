97a197b8b859471e4aea14e097398cca192bf584
Sijan04 <98251827+Sijan04@users.noreply.github.com>
2024-11-01 02:48:43 +0600

    Add files via upload

---
+++ Ternary_Condition.js
@@ -0,0 +1,7 @@
+var number= Number(prompt("Enter a number: "));
+
+
+// number > 0 ? console.log("Positive") : console.log("Negative");  positive or negative number check 
+
+var Result = number>0? "positive" : number<0 ? "negative" : "Zero";
+document.write(Result);
