66771542143e65edb19e6efb3ba724f9fd80b621
Sijan04 <98251827+Sijan04@users.noreply.github.com>
2024-10-31 15:52:59 +0600

    Add files via upload

--- Grade.js
+++ Grade.js
@@ -1,20 +1,22 @@
  //Program for Task 
 // Letter Grade Program. Happy coding
 
-var marks = parseInt(prompt("Enter Marks : "))      //parseInt convert a string into an integer.
+var marks = parseInt(prompt("Enter Marks : "));      //parseInt convert a string into an integer.
 
+if(marks >100 || marks <0)
+    document.write("Invalid Marks")
 // checking the conditions
-if (marks >= 80)
+else if (marks >= 80 && marks <=100)
     document.write("A+")
-else if (marks >= 70)
+else if (marks >= 70 && marks <=79)
     document.write("A")
-else if (marks >= 60)
+else if (marks >= 60 && marks <=69)
     document.write("A-")
-else if (marks >= 50)
+else if (marks >= 50 && marks <=59)
     document.write("B")
-else if (marks >= 40)
+else if (marks >= 40 && marks <=49)
     document.write("C")
-else if (marks >= 33)
+else if (marks >= 33 && marks <=39)
     document.write("D")
 else
     document.write("Sorry. You are Failed")
