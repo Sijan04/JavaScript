7bd646e7b7a332dff6c85045ea33d1fbe67acf26
Sijan04 <98251827+Sijan04@users.noreply.github.com>
2024-10-31 16:55:54 +0600

    Add files via upload

---
+++ Vowel.js
@@ -0,0 +1,11 @@
+var Letter =prompt("Enter a letter: ");
+
+Letter=Letter.toLocaleLowerCase();     //Convert Capital to Smaller Case
+
+if (Letter =='a' || Letter =='e' || Letter =='i' || Letter =='o' || Letter =='u' )
+
+console.log("Vowel");
+
+else
+
+console.log("Consonat");
