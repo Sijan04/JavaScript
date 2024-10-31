dc9d53704f6907242bad4ebc052a2b4e48310f47
Sijan04 <98251827+Sijan04@users.noreply.github.com>
2024-10-31 17:20:24 +0600

    Add files via upload

---
+++ Switch.js
@@ -0,0 +1,27 @@
+var digit= prompt("Enter a number: ");
+
+switch(digit)
+{
+
+ case "0":
+    document.write("Zero");
+    break;
+
+    case "1":
+    document.write("One");
+    break;
+
+
+    case "2":
+    document.write("Two");
+    break;
+
+    
+    case "3":
+    document.write("Three");
+    break;
+    
+ default:
+     document.write("Not a figit");
+
+}
