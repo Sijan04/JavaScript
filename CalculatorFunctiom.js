ebc44a0a50e6977a8a43588b1b2d33cd2dc51d0b
Sijan04 <98251827+Sijan04@users.noreply.github.com>
2024-11-02 15:55:32 +0600

    Add files via upload

---
+++ CalculatorFunctiom.js
@@ -0,0 +1,23 @@
+function addition (x,y)
+{
+    var Result = x + y;
+    document.write("Sum= " + Result + "<br>");
+}
+
+
+function Substraction (x,y)
+{
+    var Result = x - y;
+    document.write("Sub= " + Result + "<br>");
+}
+
+function Division (x,y)
+{
+    var Result = x / y;
+    document.write("Div= " + Result + "<br>");
+}
+
+addition(10,20);
+Substraction(20,5);
+Division(20,5);   //function call
+
