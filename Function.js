3a9c26ee133498352b1937afe388f6d16e005fad
Sijan04 <98251827+Sijan04@users.noreply.github.com>
2024-11-02 15:25:40 +0600

    Add files via upload

---
+++ Function.js
@@ -0,0 +1,11 @@
+
+//creating a function
+function sqaure(number1,number2){
+//var number=5;
+var Result=number1 * number2;
+//document.write("Result = " + Result + "<br/>")
+return Result;
+}
+
+//function calling
+document.write(sqaure(5,6));
