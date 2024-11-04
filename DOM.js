56256889034fa77c9ad859f4a879e39df9357cb0
Sijan04 <98251827+Sijan04@users.noreply.github.com>
2024-11-05 01:32:57 +0600

    Add files via upload

---
+++ DOM.js
@@ -0,0 +1,8 @@
+//using html then edit from javascript 
+
+
+document.getElementById("heading1").innerHTML="I am not sijan anymore";
+
+document.getElementById("heading2").innerHTML="Not a sunny day";
+
+document.getElementById("heading3").innerHTML="Form javascript";
