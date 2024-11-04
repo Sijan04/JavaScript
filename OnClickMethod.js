002b80ac04a28dc6ebf5746cb5abbb9c96cdc234
Sijan04 <98251827+Sijan04@users.noreply.github.com>
2024-11-05 02:35:26 +0600

    Add files via upload

---
+++ OnClickMethod.js
@@ -0,0 +1,25 @@
+function OnClick()
+{
+   // alert("Hello I am Button1");
+
+   var myVar = document.querySelector("#peraId");
+   myVar.innerHTML="You have click";
+
+}
+
+
+function OnClick2()
+{
+   //alert("Hello I am Button2");
+
+   var myVar = document.querySelector("#peraId");
+   myVar.innerHTML="Hello button2";
+}
+
+function OnClick3()
+{
+    // alert("I am a word");
+
+   var myVar = document.querySelector("#peraId");
+   myVar.innerHTML="Hello button 3";
+}
