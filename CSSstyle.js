a0f7c13459a6461de4d0f5acf4a86a40e317f034
Sijan04 <98251827+Sijan04@users.noreply.github.com>
2024-11-07 16:52:57 +0600

    Add files via upload

---
+++ CSSstyle.js
@@ -0,0 +1,19 @@
+function addStyle()
+{
+  
+  var myvar= document.querySelector("#peraID1");
+
+  //myvar.style.color="red";
+
+  myvar.classList.add("pera-Style");   //CSS class add
+
+}
+
+function removeStyle()
+{
+
+    var myvar= document.querySelector("#peraID1");
+
+    myvar.classList.remove("pera-Style");  //CSS class add
+
+}
