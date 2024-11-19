d4fc832c9f3eb5603167924fd925d2165e8ef31b
Sijan04 <98251827+Sijan04@users.noreply.github.com>
2024-11-19 15:47:45 +0600

    Add files via upload

---
+++ ClipboardEvent.js
@@ -0,0 +1,20 @@
+
+
+
+const input=document.querySelector("input");
+
+input.addEventListener("copy",function(){
+
+    console.log("copied");
+
+
+});
+
+
+
+input.addEventListener("cut",function(){
+
+    console.log("cut");
+
+
+});
