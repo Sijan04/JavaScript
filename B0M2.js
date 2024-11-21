29ac71d1bfa37344cc8bd0a227921772813136a7
Sijan04 <98251827+Sijan04@users.noreply.github.com>
2024-11-21 15:00:34 +0600

    Add files via upload

---
+++ B0M2.js
@@ -0,0 +1,18 @@
+//popup box-alert
+
+
+function deletesomethong()
+{
+    var value = confirm("do you want to delete?");
+
+if(value)
+{
+    console.log("delete");
+}
+else
+{
+    console.log("not delete");
+}
+}
+
+deletesomethong();
