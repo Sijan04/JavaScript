1fac72313aea1fab7044ab001dd516d780f74979
Sijan04 <98251827+Sijan04@users.noreply.github.com>
2024-11-24 17:17:16 +0600

    Add files via upload

---
+++ ErrorHandle.js
@@ -0,0 +1,23 @@
+//Try,Catch code error handle
+
+
+
+
+try{
+
+    alert("Hi Sijan");
+
+    alert(error-code); //error code
+
+}
+catch(errors)
+{
+
+    console.log(errors);
+
+}
+
+finally
+{
+    alert("Bye Sijan");
+}
